403d8578ba08626d38634f388514fb69704620e5
Abhijit Gupta <abhijit038@gmail.com>
2026-05-17 00:22:31 +0530

    Add: house-robber - submission-0

---
+++ Data Structures & Algorithms/house-robber/submission-0.py
@@ -0,0 +1,18 @@
+from functools import cache
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        # This is like arranging 0's and 1's -> you should not
+        # have two ones adjacent
+        @cache
+        def dp(i: int) -> int:
+            # think about last house
+            # if i rob this, i cannot rob (n - 1)
+            # how? use max( , )
+            if i == 1:
+                return nums[0]
+            if i == 2:
+                return max(nums[0], nums[1])
+            return max(dp(i - 2) + nums[i - 1], dp(i - 1) )
+        return dp(len(nums))
+
+        
